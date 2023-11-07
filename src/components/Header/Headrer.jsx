a56f2e5419ea45e4f92a9df2cedef12282fee41a
Mahapara <mahanizamani12345678@gmail.com>
2023-11-07 20:41:00 +0500

    changes

--- src/components/Header/Headrer.jsx
+++ src/components/Header/Headrer.jsx
@@ -1,6 +1,23 @@
 import React from "react";
-
+import { Container, Logo, LogoutBtn } from "../index";
+import { Link } from "react-router-dom";
+import { UseSelector, useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom";
 const Headrer = () => {
+  const authStatus = useSelector((state) =>   state.auth.status)
+  const navigate = useNavigate()
+  const naItems = [
+    {
+      name: 'Home',
+      slug: "/",
+      active: true
+    },
+    {
+      name: 'Login',
+      slug: "/login",
+      active: !authStatus,
+    },
+  ]
   return <div>Header</div>;
 };
 
